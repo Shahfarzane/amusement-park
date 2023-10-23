ed20fc8387df34e493a12694f8dbdb2c0ec02b1a
Shahin Farzane <42602105+Shahfarzane@users.noreply.github.com>
2023-10-23 15:42:33 +0200

    first commit

---
+++ src/components/App.tsx
@@ -0,0 +1,38 @@
+import logo from 'assets/logo.svg'
+
+const randoms = [
+  [1, 2],
+  [3, 4, 5],
+  [6, 7]
+]
+
+function App() {
+  return (
+    <div className="relative overflow-hidden bg-white">
+      <div className="h-screen sm:pb-40 sm:pt-24 lg:pb-48 lg:pt-40">
+        <div className="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8">
+          <div className="sm:max-w-lg">
+            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+              Welcome!
+            </h1>
+            <p className="mt-4 text-xl text-gray-500"></p>
+          </div>
+          <div>
+            <div className="my-10">
+              <div
+                aria-hidden="true"
+                className="pointer-events-none mt-10 md:mt-0 lg:absolute lg:inset-y-0 lg:mx-auto lg:w-full lg:max-w-7xl"
+              >
+                <div className="absolute sm:left-1/2 sm:top-0 sm:translate-x-8 lg:left-1/2 lg:top-1/2 lg:-translate-y-1/2 lg:translate-x-8">
+                  <div className="flex items-center space-x-6 lg:space-x-8"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
